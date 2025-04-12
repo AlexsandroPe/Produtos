1e903bc05d28d906ab56fb90c3f032bc11a6d764
Alexsandro Pereira <alexpereirasilv4@gmail.com>
2025-04-12 18:07:32 -0300

    removendo alguns arquivos

--- src/screens/ShoppingCart/index.js
+++ src/screens/ShoppingCart/index.js
@@ -9,9 +9,7 @@ export default function () {
     <SafeAreaView style={{ flex: 1,   backgroundColor: "rgba(178, 201, 209, 0.77)",}}>
       <Text style={{fontSize: 28, fontWeight: "700", alignSelf: "center"}}>SitUCool</Text>
       <View style={{ flex: 1, gap: 20, padding: 10}}>
-        
         <Text style={{fontSize: 28, fontWeight: "500"}}>Carrinho</Text>
-        
         <ProductCard  name={"Nordic Step"} image={require("../../assets/produtos/cadeiraMato.png")} activeIcon={true} description={"Feita com madeira tailandesa e importada da Tailândia"}  price="R$759,20" />
       </View>
     </SafeAreaView>
